f3b681a13c64d3e09e6f770624a2a4ef89e1d499
Ілля <i.cherkasov2027@pu.org.ua>
2026-06-06 14:09:32 +0300

    Виконано ПР27: додано ресурси та стилі

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 #include "mainwindow.h"
-
 #include <QApplication>
+#include <QFile>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QFile styleFile(":/new/prefix1/style.qss");
+
+    if (styleFile.open(QFile::ReadOnly)) {
+        a.setStyleSheet(styleFile.readAll());
+        styleFile.close();
+        qDebug() << "Стиль успішно застосовано!";
+    } else {
+        qDebug() << "ПОМИЛКА: Не вдалося відкрити стиль!";
+    }
+
     MainWindow w;
     w.show();
     return a.exec();
